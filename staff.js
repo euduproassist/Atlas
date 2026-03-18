8dad9b69adc09260d3b19e0ffcd9e34901a837ee
eduproassist <eduproassist44@gmail.com>
2026-03-18 09:59:00 +0200

    Update staff.js

--- staff.js
+++ staff.js
@@ -1,5 +1,5 @@
 import { auth, db } from './firebase-config.js';
-import { collection, query, onSnapshot, doc, updateDoc, orderBy } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
+import { collection, query, onSnapshot, doc, updateDoc, orderBy, getDoc } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js";
 
 const tableBody = document.getElementById('applicationTableBody');
