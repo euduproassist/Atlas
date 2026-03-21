6f0277d6e001739d75451033a8a82278521d3c3f
eduproassist <eduproassist44@gmail.com>
2026-03-21 15:00:40 +0200

    Update applicant.js

--- applicant.js
+++ applicant.js
@@ -196,7 +196,7 @@ window.changeZoom = function(amount) {
 };
 
 // Contact Support Modal for TUT Arcadia
-document.querySelector('a.q-link[href="#"]:nth-child(2)').addEventListener('click', (e) => {
+
     e.preventDefault();
     const modal = document.getElementById('statusModal');
     const body = document.getElementById('statusModalBody');
