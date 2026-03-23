0407c387fc357f0ecd1d59c0278d705a3b77254d
eduproassist <eduproassist44@gmail.com>
2026-03-23 08:25:43 +0200

    Update staff.js

--- staff.js
+++ staff.js
@@ -58,8 +58,9 @@ function loadApplications() {
             // Map data from your Student Portal structure
             const studentName = data.step1?.fullNames + " " + (data.step1?.surname || "");
             const course = data.step2?.choice1 || "Not Selected";
-             const course2 = data.step2?.choice2 || "Not Selected";
-            const status = data.status || "pending";
+            const course2 = data.step2?.choice2 || "Not Selected";
+            const status1 = data.status1 || "pending";
+            const status2 = data.status2 || "pending";
             const dateSub = data.lastUpdated ? new Date(data.lastUpdated.seconds * 1000).toLocaleDateString() : "N/A";
             
             // Format ID like the photo (APP23-001) using last 4 digits of UID
