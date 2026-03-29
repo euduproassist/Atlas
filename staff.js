0e23ff7a9e58c3e662efbe9a2c9d955ba2d649d0
eduproassist <eduproassist44@gmail.com>
2026-03-29 07:01:47 +0200

    Update staff.js

--- staff.js
+++ staff.js
@@ -71,11 +71,7 @@ document.getElementById('archivedCount').innerText = archivedCount;
     <th>Application ID</th>
     <th>Student Name</th>
     <th>Course 1 Declined</th>
-    <th>Course 2 Declined</th>
-    <th class="hide-mobile">Date Submitted</th>
-    <th>Date Declined</th>
-    <th>Declined By</th>
-    <th>Action</th>
+
     </tr>`;
         
     } else {
