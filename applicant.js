dd405464a9a2ddecb5ff94f7adbfe782a9065507
eduproassist <eduproassist44@gmail.com>
2026-04-01 07:36:48 +0200

    Update applicant.js

--- applicant.js
+++ applicant.js
@@ -145,7 +145,7 @@ document.getElementById('trackStatusBtn').addEventListener('click', async () => 
     ${currentStatus === 'review' ? 'UNDER REVIEW' : 
       currentStatus === 'waiting' ? 'WAITING LIST' : 
       currentStatus === 'uncon_accepted' ? 'UNCONDITIONALLY ACCEPTED' :
-      currentStatus === 'withdrawn_expired' ? 'WITHDRAWN / EXPIRED' :
+      currentStatus === 'withdrawn_expired' ? 'WITHDRAWN' :
       currentStatus.replace('_', ' ').toUpperCase()}
      </td>
         <td style="padding: 15px 10px;">
