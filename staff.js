3779318b8b1454db8f8e031b820a39a1ceda4d76
eduproassist <eduproassist44@gmail.com>
2026-04-01 06:11:48 +0200

    Update staff.js

--- staff.js
+++ staff.js
@@ -166,7 +166,13 @@ document.getElementById('archivedCount').innerText = archivedCount;
                     row.innerHTML = `
                     <td><strong>${displayId}</strong></td>
                     <td>${course}</td>
-                    
+                    <td>
+                   <span class="status status-${status1}">
+                        ${status1 === 'review' ? 'UNDER REVIEW' : 
+                        status1 === 'waiting' ? 'WAITING LIST' : 
+                        status1.toUpperCase()}
+                       </span>
+                    </td>
                     <td class="hide-mobile">${dateSub}</td>
                     <td>
                         <div style="display: flex; align-items: center; gap: 8px;">
