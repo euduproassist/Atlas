647c2b5bdd69bf646342815e7646860deb799d4d
eduproassist <eduproassist44@gmail.com>
2026-04-06 15:16:03 +0200

    Update staff.js

--- staff.js
+++ staff.js
@@ -399,7 +399,6 @@ let vaultHTML = `
                 <th style="padding: 10px;">Document Name</th>
                 <th style="padding: 10px;">Size</th>
                 <th style="padding: 10px;">File Name</th>
-                <th style="padding: 10px;">Action</th>
             </tr>
         </thead>
         <tbody style="font-size: 0.9rem;">`;
