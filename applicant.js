d4d02d82b5bab8339df263cb92222e3c9842ada9
eduproassist <eduproassist44@gmail.com>
2026-04-07 08:56:05 +0200

    Update applicant.js

--- applicant.js
+++ applicant.js
@@ -1,7 +1,7 @@
 import { auth, db } from './firebase-config.js';
 import { onAuthStateChanged, signOut, sendPasswordResetEmail } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js";
 import { doc, getDoc, onSnapshot, updateDoc } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
-import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-storage.js";
+import { getStorage, ref, uploadBytes, getDownloadURL, deleteObject } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-storage.js";
 
 const loader = document.getElementById('loader');
 const userNameDisplay = document.getElementById('userNameDisplay');
