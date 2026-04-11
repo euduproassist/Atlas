a4dae8f5b5db34a6dec1f8a8e15c7cf3e3f97ada
eduproassist <eduproassist44@gmail.com>
2026-04-11 10:10:21 +0200

    Update staff.js

--- staff.js
+++ staff.js
@@ -787,6 +787,14 @@ window.updateVaultStatuses = async (data) => {
     });
 
     try {
+        if (rejectedReasons.length > 0) {
+    updates.emailTrigger = {
+        recipientEmail: data.step1.email,
+        studentName: data.step1.fullNames,
+        reasons: rejectedReasons, // This sends the array the function expects
+        timestamp: new Date()
+    };
+        }
 
         await updateDoc(appRef, updates);
         alert("Documents updated and email triggered!");
