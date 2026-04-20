6f71a79aeb8f46c134b61e471a7f914c57e82427
eduproassist <eduproassist44@gmail.com>
2026-04-20 14:00:07 +0200

    Update staff.js

--- staff.js
+++ staff.js
@@ -1007,7 +1007,7 @@ if (overlap) {
     try {
     const cycleRef = await addDoc(collection(db, "application_cycles"), {
     name: name,
-    academicYear: year,
+    academicYear: acadYear,
     openDate: open,
     closingDate: close,
     createdAt: new Date()
@@ -1023,7 +1023,7 @@ for (let row of rows) {
         await addDoc(collection(db, "course_offerings"), {
             cycleId: cycleRef.id,
             cycleName: name,
-            academicYear: year,
+            academicYear: acadYear,
             course: course,
             campus: campus,
             attendanceMode: mode,
